eef3b5861b37b8da640d5481cc41d17ad2765f42
Nwabor Kelvin <knwabor@gmail.com>
2022-12-28 18:43:59 +0100

    Implemented react router and styling

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,17 +1,49 @@
 import React from "react";
-import { ReactDOM } from "react";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import User from "./UsersPage/User";
 import Bookings from "./BookingPage/Bookings";
 import Bookables from "./BookablesPage/Bookables";
 import {FaCalendarAlt, FaDoorOpen, FaUsers} from "react-icons/fa";
+import UserPicker from "./UserPicker";
 
 const App = () => {
-    return (<div className="container">
-        <User/>
-        <Bookings/>
-        <Bookables/>
-    </div>)
+    return <Router>
+        <div className="App">
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to = "/bookings" className="btn btn-header">
+                            <FaCalendarAlt/>
+                            <span>Bookings</span>
+                            </Link>
+                         
+                        </li>
+                        <li>
+                            <Link to = "/bookables" className="btn btn-header">
+                            <FaDoorOpen/>
+                            <span>Bookables</span>
+                            </Link>
+                            
+                        </li>
+                        <li>
+                            <Link to = "/users" className="btn btn-header">
+                            <FaUsers/>
+                            <span>Users</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <UserPicker/>
+            </header>
+        </div>
+        <Routes>
+       <Route path="/Users" element = {<User/>}></Route> 
+       <Route path="/Bookings" element = {<Bookings/>}></Route> 
+       <Route path="/Bookables" element = {<Bookables/>}></Route> 
+        </Routes>
+        
+    </Router>
 }
 
 export default App
---
+++ src/Component/UserPicker.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserPicker = () => {
+    return (
+    <select>
+        <option>User</option>
+    </select>
+    )
+}
+
+export default UserPicker
