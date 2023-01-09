197639512b0f0fd0d10cd6f2ae0e9c8eb2c21330
Nwabor Kelvin <knwabor@gmail.com>
2023-01-09 09:03:44 +0100

    makes the bookables to be more responsive

--- src/Component/BookablesPage/NextButton.jsx
+++ src/Component/BookablesPage/NextButton.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import data from "../../static.json";
 import { FaArrowRight } from "react-icons/fa";
 
 const Nextbutton = () => {
-  console.log(data);
   const { bookables, sessions, days } = data;
   const [group, setGroup] = useState("Kit");
   const bookablesInGroup = bookables.filter((item) => item.group === group);
   const [bookablesIndex, setBookablesIndex] = useState(0);
-
   const groups = [...new Set(bookables.map((b) => b.group))];
+
+  const bookable = bookablesInGroup[bookablesIndex];
+  const [hasDetails, setHasDetails] = useState(false);
 
   // this is what will happen when the next button is clicked
   function nextBookable() {
@@ -17,34 +18,71 @@ const Nextbutton = () => {
   }
 
   return (
-    <div>
-      <select value={group} onChange={(e) => setGroup(e.target.value)}>
-        {groups.map((g) => (
-          <option key={g} value={g}>
-            {g}
-          </option>
-        ))}
-      </select>
+    <Fragment>
+      {bookable && (
+        <div className="bookable-details">
+          <div className="item">
+            <h2>{bookable.title}</h2>
+            <span className="controls">
+              <label>
+                <input
+                  type="checkbox"
+                  checked={hasDetails}
+                  onChange={() => setHasDetails((has) => !has)}
+                />
+                Show Details
+              </label>
+            </span>
+          </div>
+          <p>{bookable.notes}</p>
+          {hasDetails && (
+            <div className="item-details">
+              <h3>Availability</h3>
+              <div className="bookable-availability">
+                <ul>
+                  {bookable.days.sort().map((d) => (
+                    <li key={d}>{days[d]}</li>
+                  ))}
+                </ul>
+                <ul>
+                  {bookable.sessions.map((s) => (
+                    <li key={s}>{sessions[s]}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      <div>
+        <select value={group} onChange={(e) => setGroup(e.target.value)}>
+          {groups.map((g) => (
+            <option key={g} value={g}>
+              {g}
+            </option>
+          ))}
+        </select>
 
-      <ul className="bookables items-list-nav">
-        {bookablesInGroup.map((item, i) => (
-          <li
-            key={item.title}
-            className={i === bookablesIndex ? "selected" : null}
-          >
-            <button className="btn" onClick={() => setBookablesIndex(i)}>
-              {item.title}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <p>
-        <button className="btn" onClick={nextBookable} autoFocus>
-          <FaArrowRight />
-          <span>Next</span>
-        </button>
-      </p>
-    </div>
+        <ul className="bookables items-list-nav">
+          {bookablesInGroup.map((item, i) => (
+            <li
+              key={item.title}
+              className={i === bookablesIndex ? "selected" : null}
+            >
+              <button className="btn" onClick={() => setBookablesIndex(i)}>
+                {item.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <p>
+          <button className="btn" onClick={nextBookable} autoFocus>
+            <FaArrowRight />
+            <span>Next</span>
+          </button>
+        </p>
+      </div>
+    </Fragment>
   );
 };
 
--- src/Component/UsersPage/UserList.jsx
+++ src/Component/UsersPage/UserList.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
-import data from "../../static.json"
+import React, { Fragment, useState } from "react";
+import data from "../../static.json";
 
 const UserList = () => {
-   const {users} = data;
-   const [usersIndex, setUsersIndex] = useState(1);
-    
-    return (
+  const { users } = data;
+  const [usersIndex, setUsersIndex] = useState(0);
+  const user = users[usersIndex];
+
+  return (
+    <Fragment>
+      <div>
+        <div className="item">
+          <h2>{user.name}</h2>
+        </div>
+        <p>{user.notes}</p>
         <ul className="users items-list-nav">
-            {users.map((item , i) => (
-                <li key={item.name} className={i===usersIndex? "selected":null}><button className="btn" onClick={() =>setUsersIndex(i)}>{item.name}</button></li>
-            ))}
-
+          {users.map((item, i) => (
+            <li
+              key={item.name}
+              className={i === usersIndex ? "selected" : null}
+            >
+              <button className="btn" onClick={() => setUsersIndex(i)}>
+                {item.name}
+              </button>
+            </li>
+          ))}
         </ul>
-    )
-}
+      </div>
+    </Fragment>
+  );
+};
 
-
-export default UserList
+export default UserList;
