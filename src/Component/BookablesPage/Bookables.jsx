c2607a45251914a4d1ec9f7bfc66e313d2ecedcb
Nwabor Kelvin <knwabor@gmail.com>
2023-05-10 17:24:59 +0100

    next button for updating the value

--- src/Component/BookablesPage/Bookables.jsx
+++ src/Component/BookablesPage/Bookables.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import data from "../../static.json";
-
+import { FaArrowRight } from "react-icons/fa";
 const { bookables } = data;
 
 const Bookables = () => {
@@ -8,19 +8,30 @@ const Bookables = () => {
   const bookableInGroup = bookables.filter((b) => b.group === group);
   const [bookableIndex, SetBookableIndex] = useState(1);
 
+  function nextBookables() {
+    SetBookableIndex((bookableIndex + 1) % bookableInGroup.length);
+  }
+
   return (
-    <ul className="bookables items-list-nav">
-      {bookableInGroup.map((b, i) => (
-        <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-          <button onClick={() => SetBookableIndex(i)} className="btn">
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="bookables items-list-nav">
+        {bookableInGroup.map((b, i) => (
+          <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+            <button onClick={() => SetBookableIndex(i)} className="btn">
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <p>
+        <button onClick={nextBookables} autoFocus className="btn">
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 };
 
 export default Bookables;
-// import ReducerPage from "./ReducerPage";
-// Remember to call the Reducer component
